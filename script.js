3bb3ee8dc3f6e0467524c4d9c66795f22e0a8ffc
Muza21 <lasha.muzashvili1@gmail.com>
2025-02-24 23:24:02 +0400

    solve assignment

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 console.log(`ba${2/undefined}a`);
 
 function numToBinary(number){
-    return number.toString(2);
+    let result = '';
+    while(number>0){
+        result+=number%2;
+        number = Math.floor(number/2)
+    }
+    return result.split('').reverse().join('');
 }
 
-console.log(numToBinary(12));
+// function numToBinary(number){
+//     return number.toString(2);
+// }
+
+
+const num = +prompt("Please enter a number:");
+
+console.log(numToBinary(num));
